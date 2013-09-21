0aef014526e86235b571824a42ac5cd27d6f3021
Junichi Ito <jit@sonicgarden.jp>
2013-09-21 15:29:16 +0900

    Retab

--- 20130921/code_breaker/ito_fujiwara/app/code_breaker.rb
+++ 20130921/code_breaker/ito_fujiwara/app/code_breaker.rb
@@ -1,29 +1,29 @@
 class CodeBreaker
-	
-	attr_reader :answer
-	
+  
+  attr_reader :answer
+  
   def self.hello
     true
   end
   
-	def initialize(answer)
-		@answer = answer
-	end
-	
-	def guess(code)
-		answer_array = @answer.to_s.split('')
-		code_array = code.to_s.split('')
-		
-		ret = []
-		code_array.each_with_index do |c, i|
-			if answer_array.include?(c)
-				if answer_array[i] == c
-					ret << '+'
-				else
-					ret << '-'
-				end
-			end
-		end
-		ret.join
-	end
+  def initialize(answer)
+    @answer = answer
+  end
+  
+  def guess(code)
+    answer_array = @answer.to_s.split('')
+    code_array = code.to_s.split('')
+    
+    ret = []
+    code_array.each_with_index do |c, i|
+      if answer_array.include?(c)
+        if answer_array[i] == c
+          ret << '+'
+        else
+          ret << '-'
+        end
+      end
+    end
+    ret.join
+  end
 end
--- 20130921/code_breaker/ito_fujiwara/spec/code_breaker_spec.rb
+++ 20130921/code_breaker/ito_fujiwara/spec/code_breaker_spec.rb
@@ -6,34 +6,34 @@ describe CodeBreaker do
   end
   
   describe '#answer' do
-    before do 
+    before do
       @code_breaker = CodeBreaker.new(1234)
     end
     it 'returns 1234' do
       expect(@code_breaker.answer).to eq 1234
     end
   end
-	
-	describe '#guess' do
-		before do
-			@code_breaker = CodeBreaker.new(1234)
-		end
-		context 'no matches' do
-			it 'returns nothing' do
-				expect(@code_breaker.guess(5555)).to eq ''
-			end
-		end
-		context '1 number correct and correct position' do
-			it 'returns one mark' do
-				expect(@code_breaker.guess(1555)).to eq '+'
-			end
-		end
-		
-		context '1 number correct and incorrect position' do
-			it 'returns one mark' do
-				expect(@code_breaker.guess(2555)).to eq '-'
-			end
-		end
-	end
-	
+  
+  describe '#guess' do
+    before do
+      @code_breaker = CodeBreaker.new(1234)
+    end
+    context 'no matches' do
+      it 'returns nothing' do
+        expect(@code_breaker.guess(5555)).to eq ''
+      end
+    end
+    context '1 number correct and correct position' do
+      it 'returns one mark' do
+        expect(@code_breaker.guess(1555)).to eq '+'
+      end
+    end
+    
+    context '1 number correct and incorrect position' do
+      it 'returns one mark' do
+        expect(@code_breaker.guess(2555)).to eq '-'
+      end
+    end
+  end
+  
 end
