03979298688df264c0972662c16ae5820afd5d91
Junichi Ito <jit@sonicgarden.jp>
2013-09-22 07:13:16 +0900

    Refactoring

--- 20130921/code_breaker/ito/app/code_breaker.rb
+++ 20130921/code_breaker/ito/app/code_breaker.rb
@@ -27,8 +27,7 @@ class CodeBreaker
     ''.tap do |mark|
       to_code_chars(code).each_with_index {|code_char, index|
         mark << '+' if @answer_chars[index].are_you_there?(code_char)
-      }.product(@answer_chars) {|code_and_answer|
-        code_char, answer_char = code_and_answer
+      }.product(@answer_chars) {|code_char, answer_char|
         mark << '-' if answer_char.are_you_there?(code_char)
       }
     end
