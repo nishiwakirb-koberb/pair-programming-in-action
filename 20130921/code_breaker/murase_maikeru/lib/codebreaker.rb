3179a7b168f08b80030cdc6a3e61daae6858010f
Yohsuke <flyingcamelspecial@gmail.com>
2013-09-21 16:03:44 +0900

    Refactoring for exact match case

--- 20130921/code_breaker/murase_maikeru/lib/codebreaker.rb
+++ 20130921/code_breaker/murase_maikeru/lib/codebreaker.rb
@@ -10,18 +10,9 @@ module Codebreaker
     end
 
     def judge(guess)
-      result = ""
-      not_matched_secret = ""
-      not_matched_guess = ""
 
-      guess.split(//).each_with_index do |c, i|
-        if @secret[i] == c
-          result += "+"
-        else
-          not_matched_secret += @secret[i]
-          not_matched_guess += c
-        end
-      end
+      not_matched_guess, not_matched_secret = remove_exact_match_chars(guess, @secret)
+      result = "+" * (guess.size - not_matched_guess.size)
 
       not_matched_guess.split(//).each_with_index do |c,i|
         if not_matched_secret.include? c
@@ -31,6 +22,18 @@ module Codebreaker
       end
 
       sort_result(result)
+    end
+
+    def remove_exact_match_chars(str1, str2)
+      removed1 = ""
+      removed2 = ""
+      str1.split(//).each_with_index do |c, i|
+        unless str2[i] == c
+          removed1 += c
+          removed2 += str2[i]
+        end
+      end
+      [removed1, removed2]
     end
 
     def sort_result(str)
