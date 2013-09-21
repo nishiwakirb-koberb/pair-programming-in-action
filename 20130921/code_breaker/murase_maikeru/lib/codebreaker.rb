0092418bf96e33f42475a809e320a49cd099e631
P. Michael Holland <maikeru.horando@gmail.com>
2013-09-21 14:35:38 +0900

    Now passes for single match, non-position sensitive

--- 20130921/code_breaker/murase_maikeru/lib/codebreaker.rb
+++ 20130921/code_breaker/murase_maikeru/lib/codebreaker.rb
@@ -6,10 +6,17 @@ module Codebreaker
     end
 
     def start(secret)
+      @secret = secret
     end
 
     def judge(guess)
-      ""
+      result = ""
+      guess.each_char do |c|
+        if @secret.include? c
+          result = "-"
+        end
+      end
+      result
     end
   end
 end
