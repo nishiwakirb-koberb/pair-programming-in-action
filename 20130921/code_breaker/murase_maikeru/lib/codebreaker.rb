b8d26716778075c2fe75023e2defc0a660227ab3
Yohsuke <flyingcamelspecial@gmail.com>
2013-09-21 15:09:31 +0900

    Corrected the order of output

--- 20130921/code_breaker/murase_maikeru/lib/codebreaker.rb
+++ 20130921/code_breaker/murase_maikeru/lib/codebreaker.rb
@@ -11,14 +11,33 @@ module Codebreaker
 
     def judge(guess)
       result = ""
+      not_matched_secret = ""
+      not_matched_guess = ""
+
       guess.split(//).each_with_index do |c, i|
         if @secret[i] == c
-          result = "+"
-        elsif @secret.include? c
-          result = "-"
+          result += "+"
+        else
+          not_matched_secret += @secret[i]
+          not_matched_guess += c
         end
       end
-      result
+
+      not_matched_guess.split(//).each_with_index do |c,i|
+        if not_matched_secret.include? c
+          result += "-"
+        end
+      end
+
+      sort_result(result)
+    end
+
+    def sort_result(str)
+      order = ["+", "-"]
+      sorted = str.split(//).sort_by do |c|
+        order.index c
+      end
+      sorted.join
     end
   end
 end
