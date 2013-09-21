44358fdf645aee573d151d61ccbbf06f04f488f4
techno-tanoC <0ca1yc3x7251t9p@ezweb.ne.jp>
2013-09-21 16:40:07 +0900

    remove comment

--- 20130921/code_breaker/ito_fujiwara/app/code_breaker.rb
+++ 20130921/code_breaker/ito_fujiwara/app/code_breaker.rb
@@ -10,31 +10,6 @@ class CodeBreaker
     @answer = answer
   end
   
-	
-  #def guess(code)
-  #  answer_array = @answer.to_s.split('')
-  #  code_array = code.to_s.split('')
-  #  
-  #  ret = []
-  #  code_array.each_with_index do |c, i|
-	#		if i > 0 && code_array[0..(i - 1)].include?(c)
-	#			if answer_array[i] == c
-	#				ret << '+'
-	#			end
-	#			next
-	#	  end
-  #    if answer_array.include?(c)
-  #      if answer_array[i] == c
-  #        ret << '+'
-  #      else
-  #        ret << '-'
-  #      end
-  #    end
-  #  end
-  #  ret.sort.join
-  #end
-	
-	
 	def guess(code)
 	  answer_array = @answer.to_s.split('')
 		code_array = code.to_s.split('')
