4da522d3c10129c75d6560eae9e533f96315a94a
Taiyo Akashi <onibakidd@gmail.com>
2013-10-19 16:56:15 +0900

    input 2:AB case

--- 20131019/maikeru_akashi/spec/seating_plan_spec.rb
+++ 20131019/maikeru_akashi/spec/seating_plan_spec.rb
@@ -11,4 +11,15 @@ describe SeatingPlan do
     let(:seating_plan) { SeatingPlan.new('1:Aa') }
     it { seating_plan.output.should eq '-' }
   end
+
+  context 'when input 2:AB' do
+    let(:seating_plan) { SeatingPlan.new('2:AB') }
+    it { seating_plan.output.should eq 'AB' }
+  end
+# 
+#   context 'when input 2:AaB' do
+#     let(:seating_plan) { SeatingPlan.new('2:AaB') }
+#     it { seating_plan.output.should eq 'B-' }
+#   end
+# 
 end
