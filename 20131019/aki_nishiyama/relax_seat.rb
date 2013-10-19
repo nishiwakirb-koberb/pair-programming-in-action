cd4c10cf1d3c9e289738e780a875f83b892ace39
Aki <aki.onetime@gmail.com>
2013-10-19 14:59:53 +0900

    fake it.

--- 20131019/aki_nishiyama/relax_seat.rb
+++ 20131019/aki_nishiyama/relax_seat.rb
@@ -5,6 +5,14 @@ class RelaxSeat
   def initialize( input )
     arr = input.split( /:/ )
     @seats = Array.new( arr[0].to_i, "-" )
+		if arr[1]
+	    @persons = arr[1].split(nil)
+	    sit @persons[0]
+	  end
+  end
+
+  def sit(person)
+		@seats[0] = person
   end
 
   def to_s
