5a38ce85a0cc2e2155d8132b1da69148873eca15
Taiyo Akashi <onibakidd@gmail.com>
2013-10-19 14:10:06 +0900

    create rspec frame

---
+++ 20131019/maikeru_akashi/lib/seating_plan.rb
@@ -0,0 +1,2 @@
+class SeatingPlan
+end
---
+++ 20131019/maikeru_akashi/spec/seating_plan_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require 'seating_plan'
+
+describe SeatingPlan do
+  context "when input 1:A" do
+    #seating_plan = SeatingPlan.new
+  end
+end
