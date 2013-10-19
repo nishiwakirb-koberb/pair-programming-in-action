3bcc3c5ce85e3aa1b56ef0bda8e6b1b3ae62c3de
P. Michael Holland <maikeru.horando@gmail.com>
2013-10-19 14:38:09 +0900

    Implement 1:A case

--- 20131019/maikeru_akashi/lib/seating_plan.rb
+++ 20131019/maikeru_akashi/lib/seating_plan.rb
@@ -1,2 +1,8 @@
 class SeatingPlan
+  def initialize input
+  end
+
+  def output
+    'A'
+  end
 end
--- 20131019/maikeru_akashi/spec/seating_plan_spec.rb
+++ 20131019/maikeru_akashi/spec/seating_plan_spec.rb
@@ -2,7 +2,8 @@ require 'rspec'
 require 'seating_plan'
 
 describe SeatingPlan do
-  context "when input 1:A" do
-    #seating_plan = SeatingPlan.new
+  context 'when input 1:A' do
+    let(:seating_plan) { SeatingPlan.new('1:A') }
+    it { seating_plan.output.should eq 'A' }
   end
 end
