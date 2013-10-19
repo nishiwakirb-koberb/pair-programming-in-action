8f9d58c2767528b5a4af2a4e3c3b72dd7fbf5729
aq2bq <pememo@gmail.com>
2013-10-19 15:14:54 +0900

    Modify

--- 20131019/terada_okada/lib/chair_rule.rb
+++ 20131019/terada_okada/lib/chair_rule.rb
@@ -5,22 +5,30 @@ class ChairRule
   attr_reader :result
 
   def initialize str
-    number, order = str.split(':')
+    number, orders = str.split(':')
     chairs = Array.new([Chair.new ] *number.to_i)
+    update_chairs(chairs, order)
     @result = chairs.map{ |c| c.state }.join
   end
 
-
-   class Chair
-    attr_reader :state
-
-    def initialize
-      @state = '-'
+  def update_chairs chairs,orders
+    orders.each do |order| do
+      chairs.update(order)
     end
+  end
 
-    def update
-    end
-   end
+
+ class Chair
+  attr_reader :state
+
+  def initialize
+    @state = '-'
+  end
+
+  def update person
+    @state = person
+  end
+ end
 
 end
 
