a226094205d2fbeb8d93f6a1e2dfa6e951c7aa9d
Kiyoka Nishiyama <kiyoka@sumibi.org>
2013-10-19 15:54:37 +0900

    Fixed indent.

--- 20131019/aki_nishiyama/relax_seat.rb
+++ 20131019/aki_nishiyama/relax_seat.rb
@@ -3,37 +3,37 @@
 class RelaxSeat
 
   def initialize( input )
-  	seats, persons = split_token(input)
+    seats, persons = split_token(input)
     
     @seats = Array.new( seats.to_i, "-" )
     parse persons
   end
 	
-	def split_token(token)
+  def split_token(token)
     seats, persons = token.split( /:/ )    
 
-		persons ||= ""
+    persons ||= ""
     persons = persons.split("")
 
-	  [seats, persons]
-	end
+    [seats, persons]
+  end
 
   def parse(series)
-  	series.each do |person|
-  		if person =~ /[A-Z]/
-	  		sit person
-	  	elsif person =~ /[a-z]/
-	  		leave person
-	  	end	  		
-  	end
+    series.each do |person|
+      if person =~ /[A-Z]/
+        sit person
+      elsif person =~ /[a-z]/
+        leave person
+      end	  		
+    end
   end
 
   def sit(person)
-		@seats[0] = person
+    @seats[0] = person
   end
 
   def leave(person)
-  	@seats[0] = "-"
+    @seats[0] = "-"
   end
 
   def to_s
--- 20131019/aki_nishiyama/spec/relex_seat_spec.rb
+++ 20131019/aki_nishiyama/spec/relex_seat_spec.rb
@@ -14,10 +14,10 @@ describe RelaxSeat, "" do
   end
 
   describe "#split_token" do
-  	it "split" do
-	    expect( RelaxSeat.new( "1:" ).split_token("1:") ).to  eq( ["1", []] )
-	    expect( RelaxSeat.new( "2:A" ).split_token("2:A") ).to  eq( ["2", ["A"]] )
-	    expect( RelaxSeat.new( "2:Aa" ).split_token("2:Aa") ).to  eq( ["2", ["A","a"]] )
-	  end
+    it "split" do
+      expect( RelaxSeat.new( "1:" ).split_token("1:") ).to  eq( ["1", []] )
+      expect( RelaxSeat.new( "2:A" ).split_token("2:A") ).to  eq( ["2", ["A"]] )
+      expect( RelaxSeat.new( "2:Aa" ).split_token("2:Aa") ).to  eq( ["2", ["A","a"]] )
+    end
   end
 end
