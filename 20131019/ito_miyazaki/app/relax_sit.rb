fe860b7b2d79d39467388babce1de6f3423d1dba
Junichi Ito <jit@sonicgarden.jp>
2013-10-19 14:53:02 +0900

    Replace tab to white space

--- 20131019/ito_miyazaki/app/relax_sit.rb
+++ 20131019/ito_miyazaki/app/relax_sit.rb
@@ -1,19 +1,19 @@
 class RelaxSit
   def come_and_go(input)
-  	chair_count = input[/\d+/].to_i
-  	@chairs = Array.new(chair_count, "-")
-  	steps = input[/[a-zA-Z]+/].chars
-  	steps.each do |step|
-  		index = find_empty_chair_index
-  		if step =~ /[A-Z]/
-  			@chairs[index] = step
-  		else
-  			@chairs[index] = "-"
-  		end
-  	end
-  	@chairs.join
+    chair_count = input[/\d+/].to_i
+    @chairs = Array.new(chair_count, "-")
+    steps = input[/[a-zA-Z]+/].chars
+    steps.each do |step|
+      index = find_empty_chair_index
+      if step =~ /[A-Z]/
+        @chairs[index] = step
+      else
+        @chairs[index] = "-"
+      end
+    end
+    @chairs.join
   end
   def find_empty_chair_index
-  	0
+    0
   end
 end
--- 20131019/ito_miyazaki/spec/relax_sit_spec.rb
+++ 20131019/ito_miyazaki/spec/relax_sit_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe RelaxSit do
   describe "#come_and_go" do
-  	context "#2" do
+    context "#2" do
       specify do
-  	    expect(RelaxSit.new.come_and_go("1:A")).to eq "A"
-  	  end
-   	end
-   	context "#3" do
+        expect(RelaxSit.new.come_and_go("1:A")).to eq "A"
+      end
+    end
+    context "#3" do
       specify do
-  	    expect(RelaxSit.new.come_and_go("1:Aa")).to eq "-"
-  	  end
-   	end
+        expect(RelaxSit.new.come_and_go("1:Aa")).to eq "-"
+      end
+    end
   end
 end
