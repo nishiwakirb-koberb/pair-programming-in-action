7ca81e41cd3882f3bc41e221c999ad1e282ab101
Yohsuke <flyingcamelspecial@gmail.com>
2013-10-19 14:09:05 +0900

    add mock of rspec

---
+++ 20131019/murase_fujiwara/spec/relax_seating_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+require File.join(File.dirname(__FILE__), '../src/relax_seating')
+
+describe "test example" do
+
+  it "should be ok" do
+    "hello".should be_a(String)
+  end
+end
