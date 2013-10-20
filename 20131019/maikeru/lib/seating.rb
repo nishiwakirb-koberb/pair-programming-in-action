fa895a04f107a7bcd9e99a2a5c90ff769c5ec471
P. Michael Holland <maikeru.horando@gmail.com>
2013-10-20 19:55:45 +0900

    Change occupy_seat method to be aware of neighboring seats

--- 20131019/maikeru/lib/seating.rb
+++ 20131019/maikeru/lib/seating.rb
@@ -1,38 +1,61 @@
+require 'pry'
+require 'pry-nav'
+
 class Seating
   attr_reader :result
   def initialize input
-    seat_count, in_out_record = input.split(':')
-    @seats = Array.new(seat_count.to_i, '-')
+    @seat_count, seating_record = input.split(':')
+    @seats = Array.new(@seat_count.to_i) { Seat.new }
 
-    in_out_record.each_char do |record|
+    seating_record.each_char do |record|
       if is_entry_record? record
         occupy_seat record
       else
         vacate_seat record
       end
     end
-    @result = @seats.join
+    @result = @seats.map { |seat| seat.occupant }.join
   end
 
   private
 
   def occupy_seat occupant
-    @seats[first_empty_seat_index] = occupant
+    virtual_seats = [Seat.new] + @seats + [Seat.new]
+    virtual_seats.each_cons(3) do |seat_block|
+      left_unoccupied = seat_block[0].unoccupied?
+      middle_unoccupied = seat_block[1].unoccupied?
+      right_unoccupied = seat_block[2].unoccupied?
+      both_unoccupied = left_unoccupied && right_unoccupied
+
+      if (middle_unoccupied && both_unoccupied)
+        seat_block[1].occupant = occupant
+        break
+      elsif (middle_unoccupied && right_unoccupied)
+        seat_block[1].occupant = occupant
+        break
+      end
+    end
   end
 
   def vacate_seat occupant
-    @seats[seat_index(occupant)] = '-'
-  end
-
-  def first_empty_seat_index
-    @seats.index('-').to_i
-  end
-
-  def seat_index record
-    @seats.index record.upcase
+    seat_to_vacate = @seats.find do
+      |seat| seat.occupant == occupant.upcase
+    end
+    seat_to_vacate.occupant = '-'
   end
 
   def is_entry_record? string
     string.upcase == string
+  end
+end
+
+class Seat
+  attr_accessor :occupant
+  def initialize
+    @occupant = '-'
+  end
+
+  def unoccupied?
+    @occupant == '-'
   end
 end
