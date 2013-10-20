20d5dcff8722f207ac5d2e74d707413983afd767
P. Michael Holland <maikeru.horando@gmail.com>
2013-10-20 16:48:24 +0900

    1:AB case passes without any code changes

--- 20131019/maikeru/spec/seating_spec.rb
+++ 20131019/maikeru/spec/seating_spec.rb
@@ -4,4 +4,5 @@ require 'seating'
 describe Seating do
   specify { expect(Seating.new('1:A').result).to eq 'A' }
   specify { expect(Seating.new('1:Aa').result).to eq '-' }
+  specify { expect(Seating.new('1:AB').result).to eq 'AB' }
 end
