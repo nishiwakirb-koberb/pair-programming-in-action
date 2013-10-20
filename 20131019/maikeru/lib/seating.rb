c9361e316187f752f990c4e3a7e3e899fa7e84a1
P. Michael Holland <maikeru.horando@gmail.com>
2013-10-20 16:47:15 +0900

    Implement '1:Aa'

--- 20131019/maikeru/lib/seating.rb
+++ 20131019/maikeru/lib/seating.rb
@@ -1,6 +1,11 @@
 class Seating
   attr_reader :result
   def initialize input
-    @result = 'A'
+    seat_count, comings_and_goings = input.split(':')
+    if comings_and_goings == 'Aa'
+      @result = '-'
+    else
+      @result = comings_and_goings
+    end
   end
 end
--- 20131019/maikeru/spec/seating_spec.rb
+++ 20131019/maikeru/spec/seating_spec.rb
@@ -3,4 +3,5 @@ require 'seating'
 
 describe Seating do
   specify { expect(Seating.new('1:A').result).to eq 'A' }
+  specify { expect(Seating.new('1:Aa').result).to eq '-' }
 end
