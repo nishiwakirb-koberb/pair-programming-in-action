ba6dac70b6ac856405bef40f1acc5f163e2bdeb8
P. Michael Holland <maikeru.horando@gmail.com>
2013-10-20 17:40:52 +0900

    Correct 1:AB test to 2:AB. Implement 2:AaB

--- 20131019/maikeru/lib/seating.rb
+++ 20131019/maikeru/lib/seating.rb
@@ -1,11 +1,17 @@
 class Seating
   attr_reader :result
   def initialize input
-    seat_count, comings_and_goings = input.split(':')
-    if comings_and_goings == 'Aa'
-      @result = '-'
-    else
-      @result = comings_and_goings
+    seat_count, in_out_record = input.split(':')
+    seats = Array.new(seat_count.to_i, '-')
+    in_out_record.each_char do |record|
+      if record.upcase == record
+        first_empty_seat_index = seats.index '-'
+        seats[first_empty_seat_index.to_i] = record
+      else
+        seat_index = seats.index record.upcase
+        seats[seat_index] = '-'
+      end
     end
+    @result = seats.join
   end
 end
--- 20131019/maikeru/spec/seating_spec.rb
+++ 20131019/maikeru/spec/seating_spec.rb
@@ -4,5 +4,6 @@ require 'seating'
 describe Seating do
   specify { expect(Seating.new('1:A').result).to eq 'A' }
   specify { expect(Seating.new('1:Aa').result).to eq '-' }
-  specify { expect(Seating.new('1:AB').result).to eq 'AB' }
+  specify { expect(Seating.new('2:AB').result).to eq 'AB' }
+  specify { expect(Seating.new('2:AaB').result).to eq 'B-' }
 end
