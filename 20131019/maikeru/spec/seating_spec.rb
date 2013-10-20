ff5b50f0b0f7a3f6b29a62bf340bc7d1536a2f3d
P. Michael Holland <maikeru.horando@gmail.com>
2013-10-20 18:01:03 +0900

    2:AZz case passes without code changes

--- 20131019/maikeru/spec/seating_spec.rb
+++ 20131019/maikeru/spec/seating_spec.rb
@@ -7,4 +7,5 @@ describe Seating do
   specify { expect(Seating.new('2:AB').result).to eq 'AB' }
   specify { expect(Seating.new('2:AaB').result).to eq 'B-' }
   specify { expect(Seating.new('2:AZa').result).to eq '-Z' }
+  specify { expect(Seating.new('2:AZz').result).to eq 'A-' }
 end
