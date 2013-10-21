1b406be082c0f660e85fd5fb3d7881a660768026
aq2bq <pememo@gmail.com>
2013-10-21 15:52:10 +0900

    Refactoring

--- 20131019/terada/lib/chair_rule.rb
+++ 20131019/terada/lib/chair_rule.rb
@@ -4,9 +4,26 @@ require 'byebug'
 class ChairRule
   attr_reader :result
 
-  def initialize str
-    @result = '------'
+  def initialize orders
+    number, people = parse orders
+    chairs = Array.new(number) { Chair.new }
+    @result = chairs.map(&:state).join
   end
 
+  def parse orders
+    [orders.split(':').first.to_i, orders.split(':').last.split(//)]
+  end
+
+  class Chair
+    attr_reader :state
+
+    def initialize
+      @state = '-'
+    end
+
+    def update person
+      @state = person
+    end
+  end
 end
 
