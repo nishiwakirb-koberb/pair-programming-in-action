709fb7d53c1b533ee425c55aedcf6b58c7131df0
aq2bq <pememo@gmail.com>
2013-10-22 21:48:59 +0900

    Refactoring

--- 20131019/terada/lib/chair_rule.rb
+++ 20131019/terada/lib/chair_rule.rb
@@ -8,8 +8,11 @@ class ChairRule
     number, people = parse orders
     @chairs = Array.new(number) { |i| Chair.new(i) }
     update_chairs people
-    @result = @chairs.map(&:person).join
+    @result = chairs.map(&:person).join
   end
+
+
+  private
 
   def parse orders
     [orders.split(':').first.to_i, orders.split(':').last.split(//)]
@@ -23,32 +26,30 @@ class ChairRule
   end
 
   def next_chair
-    only1_chair or both_side_empty or oneside_empty or last_chair or primary_side_of_chairs or empties.first
+    only1_chair or both_side_empty or oneside_empty or final_chair or primary_side_of_chairs or empties.first
   end
 
   def chairs
-    @chairs.map(&:person)
+    @chairs ||= []
   end
 
   def only1_chair
-    (@chairs.size < 3 or empties.size == 1 or empties.size == @chairs.size) ?
+    (chairs.size < 3 or empties.size == 1 or empties.size == chairs.size) ?
       empties.first : false
   end
 
   def both_side_empty
-    last_id = @chairs.size - 1
     empties.each do |chair|
-      return chair if (1..(last_id - 1)).include? chair.id and right_chair(chair).empty? and left_chair(chair).empty?
+      return chair if have_both_side?(chair.id) and right_chair(chair).empty? and left_chair(chair).empty?
     end
     false
   end
 
   def oneside_empty
-    last_id = @chairs.size - 1
     empties.each do |chair|
-      if (1..(last_id - 1)).include? chair.id
+      if have_both_side?(chair.id)
         if right_chair(chair).empty?
-          if right_chair(chair).id == last_id
+          if is_last?(right_chair(chair).id)
             return right_chair(chair)
           else
             return chair
@@ -61,20 +62,18 @@ class ChairRule
     false
   end
 
-  def last_chair
-    last_id = @chairs.size - 1
+  def final_chair
     empties.each do |chair|
-      return chair if chair.id == last_id and left_chair(chair).empty?
+      return chair if is_last?(chair.id) and left_chair(chair).empty?
     end
     false
   end
 
   def primary_side_of_chairs
-    last_id = @chairs.size - 1
     empties.each do |chair|
       if chair.id == 0
         return chair
-      elsif chair.id == last_id
+      elsif is_last?(chair.id)
         return chair
       end
     end
@@ -86,15 +85,15 @@ class ChairRule
   end
 
   def find_by_id id
-    @chairs.find{ |chair| chair.id == id }
+    chairs.find{ |chair| chair.id == id }
   end
 
   def find_by_person person
-    @chairs.find{ |chair| chair.person == person.upcase }
+    chairs.find{ |chair| chair.person == person.upcase }
   end
 
   def empties
-    @chairs.select{ |chair| chair.empty? }
+    chairs.select{ |chair| chair.empty? }
   end
 
   def right_chair chair
@@ -103,6 +102,14 @@ class ChairRule
 
   def left_chair chair
     find_by_id(chair.id - 1)
+  end
+
+  def is_last? id
+    id == chairs.size - 1
+  end
+
+  def have_both_side? id
+    (1..(chairs.size - 2)).include? id
   end
 
   class Chair
