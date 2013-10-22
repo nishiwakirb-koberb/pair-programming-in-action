d2da801dbdeac8254dc1fb136a01fdc0c97a3843
Junichi Ito <jit@sonicgarden.jp>
2013-10-22 22:42:16 +0900

    Refactoring

--- 20131019/murase_fujiwara/src/relax_seating.rb
+++ 20131019/murase_fujiwara/src/relax_seating.rb
@@ -1,15 +1,12 @@
-REGEX_ARRAY = [/(?<=-)-(?=-)/, /(?<=-)-(?=\w)|(?<=\w)-(?=-)/, /(?<=\w)-(?=\w)/]
-
-def replace(seats, c)
-  virtual_seats = "-#{seats}-"
-  
-  REGEX_ARRAY.map{|r| virtual_seats.sub(r, c) }.find{|s| s != virtual_seats}[1...-1]
-end
+REGEX_ARRAY = [/(?<=-)-(?=-)/, /(?<=-)-(?=\w)|(?<=\w)-(?=-)/, /(?<=\w)-(?=\w)/].freeze
+ESCAPE_FROM_VIRTUAL = (1...-1).freeze
 
 def relax_seating(input)
   num, sequence = input.split(':')
+  sequence.each_char.inject('-' * num.to_i) {|seats, c| c == c.upcase ? replace(seats, c) : seats.sub(c.upcase, '-') }
+end
 
-  sequence.each_char.inject("-" * num.to_i) {|seats, c|
-    c.upcase == c ? replace(seats, c) : seats.sub(c.upcase, '-')
-  }
+def replace(seats, c)
+  virtual_seats = "-#{seats}-"
+  REGEX_ARRAY.map {|r| virtual_seats.sub(r, c) }.find {|s| s != virtual_seats }[ESCAPE_FROM_VIRTUAL]
 end
