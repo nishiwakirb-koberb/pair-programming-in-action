9aae9755b5e5971c58f957aa7b8215fa549aa225
Junichi Ito <jit@sonicgarden.jp>
2013-10-22 12:33:36 +0900

    Good-bye to Seat class

--- 20131019/maikeru/lib/seating.rb
+++ 20131019/maikeru/lib/seating.rb
@@ -1,56 +1,37 @@
 class Seating
+  UNOCCUPIED = '-'.freeze
+
   def initialize input
     @seat_count, @seating_record = input.split(':')
   end
 
   def result
-    @seats = Array.new(@seat_count.to_i) { Seat.new }
+    @seats = (UNOCCUPIED * @seat_count.to_i).chars
 
     @seating_record.each_char do |record|
       is_entry_record?(record) ? occupy_seat(record) : vacate_seat(record)
     end
 
-    @seats.map {|seat| seat.occupant }.join
+    @seats.join
   end
 
   private
 
+  def is_entry_record? string
+    string.upcase == string
+  end
+
   def occupy_seat occupant
-    best_available_seat.occupy!(occupant)
+    best_available_seat.sub!(UNOCCUPIED, occupant)
   end
 
   def best_available_seat
-    [Seat.new, *@seats, Seat.new].each_cons(3).max_by {|left, center, right|
-      center.unoccupied? ? [left, right].count(&:unoccupied?) : -1
+    [UNOCCUPIED, *@seats, UNOCCUPIED].each_cons(3).max_by {|left, center, right|
+      center == UNOCCUPIED ? [left, right].count {|seat| seat == UNOCCUPIED } : -1
     }[1]
   end
 
   def vacate_seat occupant
-    @seats.find {|seat| seat.occupant == occupant.upcase }.vacate!
-  end
-
-  def is_entry_record? string
-    string.upcase == string
-  end
-end
-
-class Seat
-  UNOCCUPIED = '-'
-  attr_reader :occupant
-
-  def initialize
-    vacate!
-  end
-
-  def unoccupied?
-    @occupant == UNOCCUPIED
-  end
-
-  def vacate!
-    @occupant = UNOCCUPIED
-  end
-
-  def occupy!(occupant)
-    @occupant = occupant
+    @seats.find {|seat| seat == occupant.upcase }.sub!(/./, UNOCCUPIED)
   end
 end
