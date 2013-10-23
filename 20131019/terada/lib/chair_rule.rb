a3776fb3a3874a1943e4c5586fdd8f4ee2b879e8
Junichi Ito <jit@sonicgarden.jp>
2013-10-23 17:31:07 +0900

    Change to implicit method name

--- 20131019/terada/lib/chair_rule.rb
+++ 20131019/terada/lib/chair_rule.rb
@@ -21,19 +21,19 @@ class ChairRule
   end
 
   def next_chair
-    only_one_chair or both_sides_empty or oneside_empty or final_chair or primary_sides_of_chairs or empties.first
+    only_one_chair or both_sides_empty or oneside_empty or final_chair or primary_sides_of_chairs or empty_chairs.first
   end
 
   def only_one_chair
-    empties.first if empties.size == @chairs.size
+    empty_chairs.first if empty_chairs.size == @chairs.size
   end
 
   def both_sides_empty
-    empties.find {|chair| has_sides?(chair) and right_chair(chair).empty? and left_chair(chair).empty? }
+    empty_chairs.find {|chair| has_sides?(chair) and right_chair(chair).empty? and left_chair(chair).empty? }
   end
 
   def oneside_empty
-    empties.select {|chair| has_sides?(chair) and right_chair(chair).empty? }.each do |chair|
+    empty_chairs.select {|chair| has_sides?(chair) and right_chair(chair).empty? }.each do |chair|
       r_chair = right_chair(chair)
       return is_last?(r_chair) ? r_chair : chair
     end
@@ -41,11 +41,11 @@ class ChairRule
   end
 
   def final_chair
-    empties.find {|chair| is_last?(chair) and left_chair(chair).empty? }
+    empty_chairs.find {|chair| is_last?(chair) and left_chair(chair).empty? }
   end
 
   def primary_sides_of_chairs
-    empties.find {|chair| chair.index == 0 or is_last?(chair) }
+    empty_chairs.find {|chair| chair.index == 0 or is_last?(chair) }
   end
 
   def find_by_index index
@@ -56,7 +56,7 @@ class ChairRule
     @chairs.find {|chair| chair.person == person.upcase }
   end
 
-  def empties
+  def empty_chairs
     @chairs.select {|chair| chair.empty? }
   end
 
