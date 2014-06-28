ebdb4a51cc6ffa1c1e175b309a5991c2aa52ce35
pinzolo <pinzolo@gmail.com>
2014-06-28 14:04:27 +0900

    Add first spec

---
+++ 20140628/vending-machine/elgoog-pinzolo/spec/vending_machine_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe VendingMachine do
+  subject { VendingMachine.new }
+  describe '#insert' do
+    [10, 50, 100, 500, 1000].each do |money|
+      context "when #{money} yen is inserted" do
+        it 'works' do
+          expect { subject.insert(money) }.not_to raise_error
+        end
+      end
+    end
+  end
+  describe '#amount' do
+    context 'when inserted no money' do
+      it 'returns 0' do
+        expect(subject.amount).to eq 0
+      end
+    end
+    context 'when inserted 1 money' do
+      before { subject.insert(10) }
+      it 'returns 10' do
+        expect(subject.amount).to eq 10
+      end
+    end
+    context 'when inserted plural money' do
+      before do
+        subject.insert(10)
+        subject.insert(50)
+        subject.insert(500)
+      end
+      it 'returns 560' do
+        expect(subject.amount).to eq 560
+      end
+    end
+  end
+end
