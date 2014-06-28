f9226c14681bcae19ff1e726a59da74209f7dc73
Aki <aki.onetime@gmail.com>
2014-06-28 14:06:02 +0900

    add 2 times insert

--- 20140628/vending-machine/hiroshi_aki/spec/vending_machine_spec.rb
+++ 20140628/vending-machine/hiroshi_aki/spec/vending_machine_spec.rb
@@ -11,5 +11,12 @@ describe 'VendingMachine' do
       machine.insert 10
       expect(machine.total).to eq 10
     end
+
+    it 'insert 10,10 yen' do
+      machine.insert 10
+      machine.insert 10
+      expect(machine.total).to eq 20
+    end
+
   end
 end
