67cd0aaa4a12325f3269c565928c158aa9cb6f0e
Yuji Takaesu <taka.esu.user@gmail.com>
2014-06-28 14:09:19 +0900

    initialize first commit

---
+++ 20140628/vending-machine/spec/vending_machine_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path('../vending_machine', File.dirname(__FILE__))
+
+describe VendingMachine do
+  it "abc" do
+    VendingMachine.new.abc
+  end
+end
---
+++ 20140628/vending-machine/vending_machine.rb
@@ -0,0 +1,5 @@
+class VendingMachine
+  def abc
+    "abc"
+  end
+end
