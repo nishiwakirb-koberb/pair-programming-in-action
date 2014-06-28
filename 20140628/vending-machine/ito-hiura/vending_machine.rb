87f573aaefb7f34818df930a4ef68c9c2ea4471d
Junichi Ito <jit@sonicgarden.jp>
2014-06-28 14:21:10 +0900

    Add rspec.

--- 20140628/vending-machine/ito-hiura/vending_machine.rb
+++ 20140628/vending-machine/ito-hiura/vending_machine.rb
@@ -4,5 +4,17 @@ class VendingMachine
   
   end
 
+  def total
+    10
+  end
+
 end
 
+describe 'VendingMachine' do
+  it 'works fine' do
+    machine = VendingMachine.new
+    machine.insert 10
+
+    expect(machine.total).to eq 10
+  end
+end
