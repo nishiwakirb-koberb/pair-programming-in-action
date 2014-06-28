03a15f6b13f5962e8fccac1a23f8b410b86d693f
Yuji Takaesu <taka.esu.user@gmail.com>
2014-06-28 14:41:55 +0900

    step0

--- 20140628/vending-machine/spec/vending_machine_spec.rb
+++ 20140628/vending-machine/spec/vending_machine_spec.rb
@@ -1,7 +1,26 @@
 require File.expand_path('../vending_machine', File.dirname(__FILE__))
 
 describe VendingMachine do
-  it "abc" do
-    VendingMachine.new.abc
+  let (:vending_machine) { VendingMachine.new }
+
+  describe "#add" do
+    it "100YEN + 10YEN return 110YEN" do
+      vending_machine.add 100
+      vending_machine.add 10
+      expect(vending_machine.amount).to eq 110
+    end
+  end
+
+  describe "#repay" do
+    context "vending_machine has 100YEN" do
+      before :each do
+        vending_machine.add 100
+      end
+
+      it "return amount and empty" do
+        expect(vending_machine.repay).to eq 100
+        expect(vending_machine.amount).to eq 0
+      end
+    end
   end
 end
--- 20140628/vending-machine/vending_machine.rb
+++ 20140628/vending-machine/vending_machine.rb
@@ -1,5 +1,18 @@
 class VendingMachine
-  def abc
-    "abc"
+  attr_reader :amount
+
+  def initialize
+    @amount = 0
   end
+
+  def add(money)
+    @amount += money
+  end
+
+  def repay
+    tmp = amount
+    @amount = 0
+    tmp
+  end
+
 end
