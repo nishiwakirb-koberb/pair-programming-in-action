83cff57af11f21fa24fb5b49579898cb16201048
matsushin11 <matsu1983@gmail.com>
2014-06-28 14:27:32 +0900

    vending_machine_spec refactor

--- 20140628/vending-machine/yoshida-matsuda-yalab/spec/vending_machine_spec.rb
+++ 20140628/vending-machine/yoshida-matsuda-yalab/spec/vending_machine_spec.rb
@@ -1,42 +1,44 @@
-require './spec_helper'
+require File.expand_path('../spec_helper', __FILE__)
 
 RSpec.describe VendingMachine do
 
+  let (:vending_machine) do
+    VendingMachine.new()
+  end
+
   it "10円玉を投入できる" do
     answer = true
-    expect(VendingMachine.new().input(10)).to eq answer
+    expect(vending_machine.input(10)).to eq answer
   end
 
   it "50円玉を投入できる" do
     answer = true
-    expect(VendingMachine.new().input(50)).to eq answer
+    expect(vending_machine.input(50)).to eq answer
   end
 
   it "100円玉を投入できる" do
     answer = true
-    expect(VendingMachine.new().input(100)).to eq answer
+    expect(vending_machine.input(100)).to eq answer
   end
 
   it "5000円玉を投入できる" do
     answer = true
-    expect(VendingMachine.new().input(500)).to eq answer
+    expect(vending_machine.input(500)).to eq answer
   end
 
   it "1000円札を投入できる" do
     answer = true
-    expect(VendingMachine.new().input(1000)).to eq answer
+    expect(vending_machine.input(1000)).to eq answer
   end
 
   it "連続で投入する" do
     answer = true
-    vending_machine = VendingMachine.new();
     expect(vending_machine.input(10)).to eq answer
     expect(vending_machine.input(10)).to eq answer
   end
 
   it "投入金額の合計を取得できる" do
     answer = 60
-    vending_machine = VendingMachine.new();
     vending_machine.input(10)
     vending_machine.input(50)
     expect(vending_machine.total).to eq answer
@@ -47,7 +49,6 @@ RSpec.describe VendingMachine do
 
   it "払い戻し操作ができる" do
     answer = 60
-    vending_machine = VendingMachine.new();
     vending_machine.input(10)
     vending_machine.input(50)
     expect(vending_machine.refound).to eq answer
