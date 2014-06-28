1a0f3660d8e7d2c5c78d1ac9955e0f58a60aa8c2
matsushin11 <matsu1983@gmail.com>
2014-06-28 14:19:04 +0900

    step 0 created

--- 20140628/vending-machine/yoshida-matsuda-yalab/bin/vending_machine.rb
+++ 20140628/vending-machine/yoshida-matsuda-yalab/bin/vending_machine.rb
@@ -1,9 +1,24 @@
 
 
-
+# 自動販売機クラス
 class VendingMachine
+  def initialize
+    @total = 0
+  end
 
+  # コインを投入する
   def input(coin)
+    @total += coin
     return true
+  end
+
+  # 合計金額を返す
+  def total()
+    return @total
+  end
+
+  # 払い戻し操作を行う
+  def refound()
+    return @total
   end
 end
