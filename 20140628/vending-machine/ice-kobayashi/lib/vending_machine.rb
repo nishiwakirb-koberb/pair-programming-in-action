fe6ff44998ada857b3784a55c56e88b99f97ca87
ice <meltedise@gmail.com>
2014-06-28 14:53:53 +0900

    add #insert and it's tests (not completed yet).

--- 20140628/vending-machine/ice-kobayashi/lib/vending_machine.rb
+++ 20140628/vending-machine/ice-kobayashi/lib/vending_machine.rb
@@ -1,5 +1,10 @@
+# -*- coding: utf-8 -*-
+
 class VendingMachine
+  def insert(money)
+    case money
+    when 10, 50, 100, 500, 1000
+      0
+    end
+  end
 end
-
-
-
--- 20140628/vending-machine/ice-kobayashi/spec/lib/vending_machine_spec.rb
+++ 20140628/vending-machine/ice-kobayashi/spec/lib/vending_machine_spec.rb
@@ -1,8 +1,21 @@
+# -*- coding: utf-8 -*-
 
 require_relative '../spec_helper.rb'
 require_relative '../../lib/vending_machine.rb'
 
 describe VendingMachine do
-  pending "under construction"
-end
 
+  describe "#insert" do
+    subject { VendingMachine.new }
+
+    # 10円玉、50円玉、100円玉、500円玉、1000円札
+    it { expect(subject.insert(10)).to   eq 0 }
+    it { expect(subject.insert(50)).to   eq 0 }
+    it { expect(subject.insert(100)).to  eq 0 }
+    it { expect(subject.insert(500)).to  eq 0 }
+    it { expect(subject.insert(1000)).to eq 0 }
+
+    pending "need to check total amount and etc..."
+  end
+
+end
