4dfd10f8dca2a23ab47e2202cfa5339ce981c1c0
pinzolo <pinzolo@gmail.com>
2014-06-28 15:02:32 +0900

    Fix nest

--- 20140628/vending-machine/elgoog-pinzolo/spec/vending_machine_spec.rb
+++ 20140628/vending-machine/elgoog-pinzolo/spec/vending_machine_spec.rb
@@ -3,6 +3,7 @@ require 'vending_machine'
 
 describe VendingMachine do
   subject { VendingMachine.new }
+
   describe '#insert' do
     context 'when inserted valid money' do
       [10, 50, 100, 500, 1000].each do |money|
@@ -18,96 +19,98 @@ describe VendingMachine do
         end
       end
     end
-    describe '#amount' do
-      context 'when inserted no money' do
-        it 'returns 0' do
-          expect(subject.amount).to eq 0
-        end
-      end
-      context 'when inserted 1 money' do
-        before { subject.insert(10) }
-        it 'returns 10' do
-          expect(subject.amount).to eq 10
-        end
-      end
-      context 'when inserted plural money' do
-        before do
-          subject.insert(10)
-          subject.insert(50)
-          subject.insert(500)
-        end
-        it 'returns 560' do
-          expect(subject.amount).to eq 560
-        end
-      end
-    end
-    describe '#refund' do
-      context 'when inserted no money' do
-        it 'returns empty array' do
-          expect(subject.refund).to eq []
-        end
-      end
-      context 'when inserted 1 money' do
-        before { subject.insert(10) }
-        it 'returns array that contains 10' do
-          expect(subject.refund).to eq [10]
-        end
-        it 'amount is 0' do
-          subject.refund
-          expect(subject.amount).to eq 0
-        end
-      end
-      context 'when inserted plural money' do
-        before do
-          subject.insert(10)
-          subject.insert(50)
-          subject.insert(500)
-        end
-        it 'returns array that contains inserted conins' do
-          expect(subject.refund).to eq [10, 50, 500]
-        end
-        it 'amount is 0' do
-          subject.refund
-          expect(subject.amount).to eq 0
-        end
-      end
-    end
+  end
 
-    describe '#juice_info' do
-      context 'when initial state' do
-        it 'returns 5 colas whose price is 120 yen' do
-          expect(subject.juice_info).to eq [{name: 'cola', price: 120, amount: 5}]
-        end
+  describe '#amount' do
+    context 'when inserted no money' do
+      it 'returns 0' do
+        expect(subject.amount).to eq 0
       end
     end
+    context 'when inserted 1 money' do
+      before { subject.insert(10) }
+      it 'returns 10' do
+        expect(subject.amount).to eq 10
+      end
+    end
+    context 'when inserted plural money' do
+      before do
+        subject.insert(10)
+        subject.insert(50)
+        subject.insert(500)
+      end
+      it 'returns 560' do
+        expect(subject.amount).to eq 560
+      end
+    end
+  end
 
-    describe 'can_buy?' do
-      context 'when given drink name that is not stored in machine' do
+  describe '#refund' do
+    context 'when inserted no money' do
+      it 'returns empty array' do
+        expect(subject.refund).to eq []
+      end
+    end
+    context 'when inserted 1 money' do
+      before { subject.insert(10) }
+      it 'returns array that contains 10' do
+        expect(subject.refund).to eq [10]
+      end
+      it 'amount is 0' do
+        subject.refund
+        expect(subject.amount).to eq 0
+      end
+    end
+    context 'when inserted plural money' do
+      before do
+        subject.insert(10)
+        subject.insert(50)
+        subject.insert(500)
+      end
+      it 'returns array that contains inserted conins' do
+        expect(subject.refund).to eq [10, 50, 500]
+      end
+      it 'amount is 0' do
+        subject.refund
+        expect(subject.amount).to eq 0
+      end
+    end
+  end
+
+  describe '#juice_info' do
+    context 'when initial state' do
+      it 'returns 5 colas whose price is 120 yen' do
+        expect(subject.juice_info).to eq [{name: 'cola', price: 120, amount: 5}]
+      end
+    end
+  end
+
+  describe 'can_buy?' do
+    context 'when given drink name that is not stored in machine' do
+      it 'returns false' do
+        expect(subject.can_buy?('fanta')).to eq false
+      end
+    end
+    context 'when given dring name that is stored in machine' do
+      context 'when stock is 0' do
         it 'returns false' do
-          expect(subject.can_buy?('fanta')).to eq false
+          expect(subject.can_buy?('cola')).to eq false
         end
       end
-      context 'when given dring name that is stored in machine' do
-        context 'when stock is 0' do
+      context 'when stock is not 0' do
+        context 'when have been inserted enough money' do
+          before do
+            subject.insert(100)
+            subject.insert(100)
+          end
+          it 'returns true' do
+            expect(subject.can_buy?('cola')).to eq true
+          end
+        end
+        context 'when have not been inserted encoding money' do
+          before { subject.insert(100) }
           it 'returns false' do
             expect(subject.can_buy?('cola')).to eq false
-          end
-        end
-        context 'when stock is not 0' do
-          context 'when have been inserted enough money' do
-            before do
-              subject.insert(100)
-              subject.insert(100)
-            end
-            it 'returns true' do
-              expect(subject.can_buy?('cola')).to eq true
-            end
-          end
-          context 'when have not been inserted encoding money' do
-            before { subject.insert(100) }
-            it 'returns false' do
-              expect(subject.can_buy?('cola')).to eq false
-            end
           end
         end
       end
