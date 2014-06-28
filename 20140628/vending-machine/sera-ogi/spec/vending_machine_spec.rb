864af9408b0a88fe698726600703d9f15c474c07
macera <ocak.kn@gmail.com>
2014-06-28 15:32:58 +0900

    扱えないお金を対応しようとしていた..

--- 20140628/vending-machine/sera-ogi/spec/vending_machine_spec.rb
+++ 20140628/vending-machine/sera-ogi/spec/vending_machine_spec.rb
@@ -1,6 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/../vending_machine')
 
 describe VendingMachine do
+  let(:moneys){[10, 50, 100, 500, 1000]}
+  let(:bad_moneys){[1, 5, 2000, 5000, 10000]}
   describe 'step0' do
 
   	before do
@@ -34,7 +36,6 @@ describe VendingMachine do
     end
 
   	it 'n円を1枚入れたら総額n円となっていること' do
-      moneys = [10, 50, 100, 500, 1000]
       moneys.each do |money|
         vm = VendingMachine.new
         vm.put_money= money
@@ -43,7 +44,6 @@ describe VendingMachine do
     end
 
   	it 'n円を2枚入れたら総額n*2円となっていること' do
-      moneys = [10, 50, 100, 500, 1000]
       moneys.each do |money|
         vm = VendingMachine.new
         vm.put_money= money
@@ -51,12 +51,41 @@ describe VendingMachine do
         expect(vm.total_money).to eq money*2
       end
     end
-
-    it '総額n円を払い戻すとn円戻ってくる' do
-      @vm.put_money= 10
-      @vm.put_money= 10
-      expect(@vm.return_money).to eq 20
+    describe '払い戻す' do
+      before do
+        @vm.put_money= 10
+        @vm.put_money= 10
+        @vm.return_money
+      end
+      it '総額n円を払い戻すとn円戻ってくる' do
+        expect(@vm.returned_money).to eq 20
+      end
+      it '総額が0円になっていること' do
+        expect(@vm.total_money).to eq 0
+      end
     end
+  end
+
+  describe 'step1' do
+    describe '１円玉、５円玉、千円札以外のお札を投入した' do
+      before do
+        @vm = VendingMachine.new
+      end
+      it '投入金額に加算しないこと' do
+        bad_moneys.each do |money|
+          @vm.put_money= money
+        end
+        expect(@vm.total_money).to eq 0
+       end
+       it '投入金額と釣り銭が同じであること' do
+         bad_moneys.each do |money|
+           @vm.put_money= money
+         end
+         expect(@vm.returned_money).to eq bad_moneys.inject(:+)
+
+       end
+    end
+
   end
 
 end
--- 20140628/vending-machine/sera-ogi/vending_machine.rb
+++ 20140628/vending-machine/sera-ogi/vending_machine.rb
@@ -1,12 +1,20 @@
 class VendingMachine
-  attr_accessor :money
+  attr_accessor :money, :returned_money
+  MONEYS = [10, 50, 100, 500, 1000]
+  BADMONEYS = [1, 5, 2000, 5000, 10000]
 
   def initialize
     @money = 0
   end
 
   def put_money= x
-    @money += x
+  	case x
+  	when *MONEYS
+  	  @money += x
+  	when *BADMONEYS
+  	  @returned_money += x
+  	end
+
   end
 
   def total_money
@@ -14,6 +22,8 @@ class VendingMachine
   end
 
   def return_money
-    @money
+    @returned_money += @money
+    @money = 0
   end
+
 end
