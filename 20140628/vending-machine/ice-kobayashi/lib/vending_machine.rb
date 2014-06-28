b6f8087cc42ed06583f92b621686eb9dcae36cc9
shoji-k <koba.s0125@gmail.com>
2014-06-28 14:33:48 +0900

    add an empty VendingMachine class

---
+++ 20140628/vending-machine/ice-kobayashi/lib/vending_machine.rb
@@ -0,0 +1,5 @@
+class VendingMachine
+end
+
+
+
--- 20140628/vending-machine/ice-kobayashi/spec/lib/vending_machine_spec.rb
+++ 20140628/vending-machine/ice-kobayashi/spec/lib/vending_machine_spec.rb
@@ -1,4 +1,8 @@
-describe "VendingMachine" do
+
+require_relative '../spec_helper.rb'
+require_relative '../../lib/vending_machine.rb'
+
+describe VendingMachine do
   pending "under construction"
 end
 
